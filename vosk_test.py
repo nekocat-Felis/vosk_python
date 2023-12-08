e59d70c3b3329c8b6d55df5f9f05da19a471c891
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-12-08 19:01:24 +0900

    commit

--- vosk_test.py
+++ vosk_test.py
@@ -1,17 +1,21 @@
 import vosk, wave, json
 
-voice = wave.open("test-voice.wav", "rb")
-model = vosk.Model("vosk-model-small-ja-0.22")
-samplerate = voice.getframerate()
-rec = vosk.KaldiRecognizer(model, samplerate)
-rec.SetWords(True)
+def main():
+    voice = wave.open("test-voice.wav", "rb")
+    model = vosk.Model("vosk-model-small-ja-0.22")
+    samplerate = voice.getframerate()
+    rec = vosk.KaldiRecognizer(model, samplerate)
+    rec.SetWords(True)
 
-while True:
-    data = voice.readframes(4000)
-    if len(data) == 0:
-        break
-    if rec.AcceptWaveform(data):
-        output = rec.Result()
-        json_dict = json.loads(output)
+    while True:
+        data = voice.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            output = rec.Result()
+            json_dict = json.loads(output)
 
-print(json.loads(rec.FinalResult())['text'])
+    print(json.loads(rec.FinalResult())['text'])
+
+if __name__ == "__main__":
+    main()
